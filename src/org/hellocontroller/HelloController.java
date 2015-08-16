b932375fb68bfd2656efbf6d105a0118cd406590
admin <admin@gmail.com>
2015-08-16 18:29:34 +0200

    Annotations @RequestMapping in a class. Lesson 8 (gontuseries)

--- src/org/hellocontroller/HelloController.java
+++ src/org/hellocontroller/HelloController.java
@@ -5,14 +5,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/pages")
 public class HelloController {
 
-	@RequestMapping("/pages/welcome")
+	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() {
 
 		ModelAndView modelAndView = new ModelAndView("HelloPage");
 		modelAndView.addObject("welcomeMessage",
-				"Hi, user, welcome to the first spring application");
+		        "Hi, user, welcome to the first spring application");
+
+		return modelAndView;
+	}
+
+	@RequestMapping("/hi")
+	public ModelAndView hiWorld() {
+
+		ModelAndView modelAndView = new ModelAndView("HelloPage");
+		modelAndView
+		.addObject("welcomeMessage",
+				"Hi, user, welcome to the first spring application. Method HI.");
 
 		return modelAndView;
 	}
