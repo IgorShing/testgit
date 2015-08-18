0ccdac7416dc1fb34890d19e057d1114ee156eb3
unknown <Igor_Shingaryov@EPBYMINW5157.minsk.epam.com>
2015-08-18 09:27:43 +0300

    Spring MVC Tutorials 15 - @InitBinder annotation, WebDataBinder, CustomDateEditor (concept) InitBinder

--- src/org/student/StudentAdmissionController.java
+++ src/org/student/StudentAdmissionController.java
@@ -7,6 +7,8 @@ import java.util.GregorianCalendar;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +19,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class StudentAdmissionController {
 
 	private Calendar calendar = new GregorianCalendar();
+
+	// Is used to exclude some fields from binding by Spring
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		binder.setDisallowedFields(new String[] {"studentMobile"});
+	}
 
 	@RequestMapping(value = "/admissionForm", method = RequestMethod.GET)
 	public ModelAndView getAdmissionForm(){
