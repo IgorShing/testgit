08257bc2e6b239e698400c83816b03784dbe1f21
unknown <Igor_Shingaryov@EPBYMINW5157.minsk.epam.com>
2015-08-21 10:31:08 +0300

    Spring MVC Tutorials 24 - Interceptor 02 ( more on understanding basics of it...)

--- src/org/system/DayOfWeekBasedAccessInterceptor.java
+++ src/org/system/DayOfWeekBasedAccessInterceptor.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class DayOfWeekBasedAccessInterceptor extends HandlerInterceptorAdapter{
@@ -24,5 +25,21 @@ public class DayOfWeekBasedAccessInterceptor extends HandlerInterceptorAdapter{
 
 		return true;
 	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("DayOfWeekBasedAccessInterceptor: Spring MVC called postHandle method" + request.getRequestURI().toString());
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+					throws Exception {
+		System.out.println("DayOfWeekBasedAccessInterceptor: Spring MVC called afterCompletion method" + request.getRequestURI().toString());
+	}
+
+
 
 }
